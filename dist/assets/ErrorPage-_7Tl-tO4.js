3c6a55bf6ad3f32054b43407c324ce2ae72d69f9
enduraew <enduraew@gmail.com>
2024-01-12 16:49:58 +0500

    sd

--- dist/assets/ErrorPage-_7Tl-tO4.js
+++
@@ -1,1 +0,0 @@
-import{e as s,i as o,j as r}from"./index-P0NPWkep.js";const i=()=>{const e=s();return o(e)?r.jsx("div",{className:"error-container",children:r.jsxs("div",{id:"error-page",children:[r.jsx("h1",{children:"Oops!"}),r.jsx("p",{children:"Sorry, an unexpected error has occurred."}),r.jsx("p",{children:r.jsx("i",{children:e.statusText||e.message})})]})}):null};export{i as default};
--- dist/assets/NotFound-sBiSorTd.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-P0NPWkep.js";const s="_root_1rdzf_1",r={root:s},t=()=>o.jsxs("div",{className:r.root,children:[o.jsx("h1",{children:"Упс!"}),o.jsx("p",{children:"Ничего не найдено!"}),o.jsx("span",{children:"Извините, но такой страницы не существует"})]}),c=()=>o.jsx(t,{});export{c as default};
